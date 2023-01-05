598f15d8f0bb3ca708b054dc9619e723eff908fe
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-05 19:02:30 +0100

    Update: added user router

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import express, { Request, Response } from 'express';
 import { getAllCoffees } from './controllers/coffeeController';
 const app = express();
 
-import pool from './db';
 import coffeeRouter from './routes/coffeeRoute';
 import originsRouter from './routes/originsRoutes';
+import userRouter from './routes/userRoutes';
 
 app.use(express.json());
 
 app.use('/origins', originsRouter);
 app.use('/coffee', coffeeRouter);
+app.use('/user', userRouter);
 
 app.listen('3001', (): void => {
   console.log('Server Running!');
