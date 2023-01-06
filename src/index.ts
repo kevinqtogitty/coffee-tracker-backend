6147254a0c575736e67c166c102148dc1d27ef2f
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-06 22:09:31 +0100

    Update: added cors and error handler

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from 'express';
-import { getAllCoffees } from './controllers/coffeeController';
 const app = express();
+import cors from 'cors';
 
 import coffeeRouter from './routes/coffeeRoute';
 import originsRouter from './routes/originsRoutes';
 import userRouter from './routes/userRoutes';
+import errorHandler from './middleware/errorHandler';
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/origins', originsRouter);
 app.use('/coffee', coffeeRouter);
 app.use('/user', userRouter);
+
+app.use(errorHandler);
 
 app.listen('3001', (): void => {
   console.log('Server Running!');
