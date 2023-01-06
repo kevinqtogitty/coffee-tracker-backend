637902f8f55312f656841929f8fb42c9eae670f5
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-06 22:09:09 +0100

    Update: try catch moved to HOF

--- src/controllers/originsController.ts
+++ src/controllers/originsController.ts
@@ -2,25 +2,14 @@ import { Request, Response } from 'express';
 import pool from '../db';
 
 const getAllOrigins = async (req: Request, res: Response) => {
-  try {
-    const origins = await pool.query('SELECT * FROM origin;');
-    res.json(origins.rows);
-    console.log(origins.rows);
-  } catch (error) {
-    console.log(error);
-  }
+  const origins = await pool.query('SELECT * FROM origin;');
+  res.status(200).json(origins.rows);
 };
 
 const getSingleOrigin = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const origin = await pool.query('SELECT * FROM origin WHERE id = $1;', [
-      id
-    ]);
-    res.json(origin.rows[0]);
-  } catch (error) {
-    console.log(error);
-  }
+  const { id } = req.params;
+  const origin = await pool.query('SELECT * FROM origin WHERE id = $1;', [id]);
+  res.status(200).json(origin.rows[0]);
 };
 
 export { getAllOrigins, getSingleOrigin };
