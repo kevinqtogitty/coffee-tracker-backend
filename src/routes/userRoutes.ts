7737b862e0ccb74d4bf7b0623962ff16d3cb616c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:41:20 +0100

    Update: request params id changed to userId

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -12,8 +12,8 @@ const userRouter = Router();
 
 userRouter.post('/', tryCatch(createAUser)); // create a user
 userRouter.get('/', tryCatch(getAllUsers)); // get all users
-userRouter.get('/id', tryCatch(getASingleUser)); // get single user
-userRouter.put('/:id/:type', tryCatch(updateUserData)); // update user data
-userRouter.delete('/:id', tryCatch(deleteASingleUser)); // delete single user
+userRouter.get('/:userId', tryCatch(getASingleUser)); // get single user
+userRouter.put('/:userId/:type', tryCatch(updateUserData)); // update user data
+userRouter.delete('/:userId', tryCatch(deleteASingleUser)); // delete single user
 
 export default userRouter;
