b747fa4ab70f0064ed03fcf8c7956e56f81da631
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:42:26 +0100

    Update: all queries moved to a single file

--- src/controllers/originsController.ts
+++ src/controllers/originsController.ts
@@ -1,14 +1,15 @@
-import { Request, Response } from 'express';
 import pool from '../db';
+import { Request, Response } from 'express';
+import { queryGetAllOrigins, queryGetSingleOrigin } from '../queries/queries';
 
 const getAllOrigins = async (req: Request, res: Response) => {
-  const origins = await pool.query('SELECT * FROM origin;');
+  const origins = await pool.query(`${queryGetAllOrigins}`);
   res.status(200).json(origins.rows);
 };
 
 const getSingleOrigin = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const origin = await pool.query('SELECT * FROM origin WHERE id = $1;', [id]);
+  const origin = await pool.query(`${queryGetSingleOrigin}`, [id]);
   res.status(200).json(origin.rows[0]);
 };
 
