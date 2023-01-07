264b3f3652c9f5eaaedb293aafae2fc22463e0e7
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:42:55 +0100

    Update: added processes and roastLevel routes

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import express, { Request, Response } from 'express';
 const app = express();
 import cors from 'cors';
 
-import coffeeRouter from './routes/coffeeRoute';
+import coffeeRouter from './routes/coffeeRoutes';
 import originsRouter from './routes/originsRoutes';
 import userRouter from './routes/userRoutes';
 import errorHandler from './middleware/errorHandler';
+import processesRouter from './routes/processesRoutes';
+import roastLevelRouter from './routes/roastLevelRoutes';
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/roastLevel', roastLevelRouter);
+app.use('/processes', processesRouter);
 app.use('/origins', originsRouter);
 app.use('/coffee', coffeeRouter);
 app.use('/user', userRouter);
