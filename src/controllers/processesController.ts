202e7c1cd49df7da89f2349115d664fef7886efb
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:42:19 +0100

    Initial commit

---
+++ src/controllers/processesController.ts
@@ -0,0 +1,10 @@
+import pool from '../db';
+import { Response, Request } from 'express';
+import { queryGetAllProcesses } from '../queries/queries';
+
+const getAllProcesses = async (req: Request, res: Response) => {
+  const coffeeProcesses = await pool.query(`${queryGetAllProcesses}`);
+  res.status(200).json(coffeeProcesses.rows);
+};
+
+export { getAllProcesses };
