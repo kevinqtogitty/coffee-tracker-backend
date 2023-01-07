79f885b1582a843b39ffe547bb168c50209c409e
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:42:08 +0100

    Update: all queries moved to a single file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,27 +1,36 @@
 import { Request, Response } from 'express';
 import pool from '../db';
-import { User } from '../types/interfaces';
+import {
+  queryCreateAUser,
+  queryDeleteAUser,
+  queryGetAllUsers,
+  queryGetASingleUser,
+  queryUpdateUserEmail,
+  queryUpdateUserFirstName,
+  queryUpdateUserLastName
+} from '../queries/queries';
 
 const createAUser = async (req: Request, res: Response) => {
   const { firstName, lastName, userId, email } = req.body;
-  const userCreated = await pool.query(
-    'INSERT INTO coffee_user (id, first_name, last_name, email) VALUES ($1, $2, $3, $4);',
-    [userId, firstName, lastName, email]
-  );
-  res.status(200).json(userCreated);
+  const userCreated = await pool.query(`${queryCreateAUser}`, [
+    userId,
+    firstName,
+    lastName,
+    email
+  ]);
+  res.status(201).json(userCreated);
 };
 
 const getAllUsers = async (req: Request, res: Response) => {
-  const users = await pool.query('SELECT * FROM coffee_user');
-  res.status(200).json(users);
+  const users = await pool.query(`${queryGetAllUsers}`);
+  res.status(200).json(users.rows);
 };
 
 const getASingleUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
-  const user = await pool.query('SELECT * FROM coffee_user WHERE id = $1', [
-    userId
-  ]);
-  res.status(200).json(user);
+  console.log(userId);
+  const user = await pool.query(`${queryGetASingleUser}`, [`${userId}`]);
+  res.status(200).json(user.rows[0]);
 };
 
 const updateUserData = async (req: Request, res: Response) => {
@@ -30,22 +39,22 @@ const updateUserData = async (req: Request, res: Response) => {
   let updatedUser;
   switch (type) {
     case 'email':
-      updatedUser = pool.query(
-        'UPDATE coffee_user SET email = $1 WHERE id = $2',
-        [data.dataToChange, userId]
-      );
+      updatedUser = pool.query(`${queryUpdateUserEmail}`, [
+        data.dataToChange,
+        userId
+      ]);
       break;
     case 'firstName':
-      updatedUser = pool.query(
-        'UPDATE coffee_user SET first_name = $1 WHERE id = $2',
-        [data.dataToChange, userId]
-      );
+      updatedUser = pool.query(`${queryUpdateUserFirstName}`, [
+        data.dataToChange,
+        userId
+      ]);
       break;
     case 'lastName':
-      updatedUser = pool.query(
-        'UPDATE coffee_user SET lastName = $1 WHERE id = $2',
-        [data.dataToChange, userId]
-      );
+      updatedUser = pool.query(`${queryUpdateUserLastName}`, [
+        data.dataToChange,
+        userId
+      ]);
       break;
     default:
       break;
@@ -54,10 +63,7 @@ const updateUserData = async (req: Request, res: Response) => {
 };
 const deleteASingleUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
-  const userDeleted = await pool.query(
-    'DELETE FROM COFFEE WHERE id = $1 RETURN *',
-    [userId]
-  );
+  const userDeleted = await pool.query(`${queryDeleteAUser}`, [userId]);
   res.status(200).json(userDeleted);
 };
 
