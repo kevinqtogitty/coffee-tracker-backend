260485481f25031c69fc93bf8578a8e29c819f9e
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:42:15 +0100

    Initial commit

---
+++ src/controllers/roastLevelController.ts
@@ -0,0 +1,10 @@
+import pool from '../db';
+import { Response, Request } from 'express';
+import { queryGetAllRoastLevels } from '../queries/queries';
+
+const getAllRoastLevels = async (req: Request, res: Response) => {
+  const roastLevels = await pool.query(`${queryGetAllRoastLevels}`);
+  res.status(200).json(roastLevels.rows);
+};
+
+export { getAllRoastLevels };
