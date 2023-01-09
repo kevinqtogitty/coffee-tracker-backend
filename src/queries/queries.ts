568598dbc948ee0bd6d398e7dbf3912d87dc1d1d
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-09 19:17:27 +0100

    Update: added update coffee record query

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -12,21 +12,13 @@ const queryDeleteAUser = 'DELETE FROM COFFEE WHERE id = $1 RETURN *;';
 
 // COFFEE queries
 const queryGetAllUsersCoffees =
-  'SELECT coffee.coffee_name AS "name", coffee.single_origin AS "singleOrigin", coffee.price AS "price", coffee.roaster AS "roaster", roast_levels.roast_level AS "roastLevel", processes.process_name AS "process", origin.country AS "country", coffee.notes AS "notes" FROM coffee_user INNER JOIN coffee ON coffee_user.id = coffee.user_id INNER JOIN roast_levels ON roast_levels.id = coffee.roast_level_id INNER JOIN processes ON processes.id = coffee.process_id INNER JOIN origin ON origin.id = coffee.origin_id WHERE coffee_user.id = $1;';
+  'SELECT coffee.id, coffee.coffee_name AS "name", coffee.single_origin AS "singleOrigin", coffee.price AS "price", coffee.roaster AS "roaster", roast_levels.roast_level AS "roastLevel", processes.process_name AS "process", origin.country AS "country", coffee.notes AS "notes" FROM coffee_user INNER JOIN coffee ON coffee_user.id = coffee.user_id INNER JOIN roast_levels ON roast_levels.id = coffee.roast_level_id INNER JOIN processes ON processes.id = coffee.process_id INNER JOIN origin ON origin.id = coffee.origin_id WHERE coffee_user.id = $1;';
 const queryGetASingleCoffee = 'SELECT * FROM coffee WHERE id = $1;';
 const queryAddANewCoffee =
   'INSERT INTO coffee (coffee_name, single_origin, price, farmer_id, origin_id, roaster, process_id, roast_level_id, user_id) values( $1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;';
 const queryDeleteACoffee = 'DELETE FROM COFFEE WHERE id = $1;';
-const queryUpdateCoffeeName =
-  'UPDATE coffee SET coffee_name = $1 WHERE id = $2;';
-const queryUpdateSingleOrigin =
-  'UPDATE coffee SET single_origin = $1 WHERE id = $2;';
-const queryUpdateRoast = 'UPDATE coffee SET roast_level_id = $1 WHERE id = $2;';
-const queryUpdateProcess = 'UPDATE coffee SET process_id = $1 WHERE id = $2;';
-const queryUpdatePrice = 'UPDATE coffee SET price = $1 WHERE id = $2;';
-const queryUpdateFarmer = 'UPDATE coffee SET farmer_id = $1 WHERE id = $2;';
-const queryUpdateOrigin = 'UPDATE coffee SET origin_id = $1 WHERE id = $2;';
-const queryUpdateRoaster = 'UPDATE coffee SET roaster = $1 WHERE id = $2;';
+const queryUpdateCoffee =
+  'UPDATE coffee SET coffee_name = $1, single_origin = $2, price = $3, farmer_id = $4, origin_id = $5, roaster = $6, process_id = $7, roast_level_id = $8, notes = $9, purchase_date = $10 WHERE id = $11';
 
 // ORIGIN queries
 const queryGetAllOrigins = 'SELECT * FROM origin;';
@@ -48,16 +40,9 @@ export {
   queryUpdateUserLastName,
   queryAddANewCoffee,
   queryDeleteACoffee,
+  queryUpdateCoffee,
   queryGetASingleCoffee,
   queryGetAllUsersCoffees,
-  queryUpdateFarmer,
-  queryUpdateOrigin,
-  queryUpdatePrice,
-  queryUpdateProcess,
-  queryUpdateRoast,
-  queryUpdateRoaster,
-  queryUpdateSingleOrigin,
-  queryUpdateCoffeeName,
   queryGetAllOrigins,
   queryGetSingleOrigin,
   queryGetAllProcesses,
