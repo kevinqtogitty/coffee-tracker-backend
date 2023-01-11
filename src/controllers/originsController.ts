8cbcff8188fabbc9e0363ebe60789f9d56fe93ad
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-11 11:38:33 +0100

    Update: added !ok response check

--- src/controllers/originsController.ts
+++ src/controllers/originsController.ts
@@ -4,12 +4,14 @@ import { queryGetAllOrigins, queryGetSingleOrigin } from '../queries/queries';
 
 const getAllOrigins = async (req: Request, res: Response) => {
   const origins = await pool.query(`${queryGetAllOrigins}`);
+  if (!origins.ok) throw new Error(`HTTP error! status: ${origins.status}`);
   res.status(200).json(origins.rows);
 };
 
 const getSingleOrigin = async (req: Request, res: Response) => {
   const { id } = req.params;
   const origin = await pool.query(`${queryGetSingleOrigin}`, [id]);
+  if (!origin.ok) throw new Error(`HTTP error! status: ${origin.status}`);
   res.status(200).json(origin.rows[0]);
 };
 
