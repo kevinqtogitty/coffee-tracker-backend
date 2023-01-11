5c5b89305ebadd50bcd14b681846e4766bde276d
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-11 11:38:16 +0100

    Update: added !ok response check

--- src/controllers/roastLevelController.ts
+++ src/controllers/roastLevelController.ts
@@ -4,6 +4,9 @@ import { queryGetAllRoastLevels } from '../queries/queries';
 
 const getAllRoastLevels = async (req: Request, res: Response) => {
   const roastLevels = await pool.query(`${queryGetAllRoastLevels}`);
+  if (!roastLevels.ok)
+    throw new Error(`HTTP error! status: ${roastLevels.status}`);
+
   res.status(200).json(roastLevels.rows);
 };
 
