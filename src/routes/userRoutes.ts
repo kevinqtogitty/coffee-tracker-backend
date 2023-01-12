b01685fed54df1642f83f370981b287c7697619c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-12 12:07:24 +0100

    Update removed type query params for get single user route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -13,7 +13,7 @@ const userRouter = Router();
 userRouter.post('/', tryCatch(createAUser)); // create a user
 userRouter.get('/', tryCatch(getAllUsers)); // get all users
 userRouter.get('/:userId', tryCatch(getASingleUser)); // get single user
-userRouter.put('/:userId/:type', tryCatch(updateUserData)); // update user data
+userRouter.put('/:userId', tryCatch(updateUserData)); // update user data
 userRouter.delete('/:userId', tryCatch(deleteASingleUser)); // delete single user
 
 export default userRouter;
