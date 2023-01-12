9f0a0081b9bb8b2a0b4252f5dbee0d4c5b6a2af2
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-12 12:08:42 +0100

    Update: removed error check which caused issues

--- src/controllers/processesController.ts
+++ src/controllers/processesController.ts
@@ -4,8 +4,6 @@ import { queryGetAllProcesses } from '../queries/queries';
 
 const getAllProcesses = async (req: Request, res: Response) => {
   const coffeeProcesses = await pool.query(`${queryGetAllProcesses}`);
-  if (!coffeeProcesses.ok)
-    throw new Error(`HTTP error! status: ${coffeeProcesses.status}`);
 
   res.status(200).json(coffeeProcesses.rows);
 };
