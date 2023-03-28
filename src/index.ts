74730b4833efa32d7f0f0c4af7123865c018e5e7
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-27 20:20:09 -0400

    Update: Moved redis client instancing into file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express';
-const app = express();
 import cors from 'cors';
+import express from 'express';
+import { createClient } from 'redis';
 
 import coffeeRouter from './routes/coffeeRoutes';
 import originsRouter from './routes/originsRoutes';
@@ -8,6 +8,10 @@ import userRouter from './routes/userRoutes';
 import errorHandler from './middleware/errorHandler';
 import processesRouter from './routes/processesRoutes';
 import roastLevelRouter from './routes/roastLevelRoutes';
+
+export const client = createClient();
+export const expiration = 3600;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -17,9 +21,16 @@ app.use('/processes', processesRouter);
 app.use('/origins', originsRouter);
 app.use('/coffee', coffeeRouter);
 app.use('/user', userRouter);
-
 app.use(errorHandler);
 
 app.listen('3001', (): void => {
   console.log('Server Running!');
+});
+
+client.connect().then(() => {
+  console.log('connected');
+});
+
+client.on('error', (err) => {
+  console.log('Error ' + err);
 });
