92b430c0957af7dedde01ad63e1a084afa4c5953
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-30 21:54:18 -0400

    Update: added redis cache for roast levels data

--- src/controllers/roastLevelController.ts
+++ src/controllers/roastLevelController.ts
@@ -1,11 +1,25 @@
 import pool from '../db';
 import { Response, Request } from 'express';
 import { queryGetAllRoastLevels } from '../queries/queries';
+import { client, expiration } from '..';
 
 const getAllRoastLevels = async (req: Request, res: Response) => {
-  const roastLevels = await pool.query(`${queryGetAllRoastLevels}`);
-
-  res.status(200).json(roastLevels.rows);
+  try {
+    const cachedRoastLevels = await client.get('roastLevels');
+    if (!cachedRoastLevels) {
+      const roastLevels = await pool.query(`${queryGetAllRoastLevels}`);
+      await client.setEx(
+        'roastLevels',
+        expiration,
+        JSON.stringify(roastLevels.rows)
+      );
+      res.status(200).json(roastLevels.rows);
+    } else {
+      res.status(200).json(JSON.parse(cachedRoastLevels));
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getAllRoastLevels };
