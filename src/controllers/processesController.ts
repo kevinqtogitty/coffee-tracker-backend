be2dfce3819d3fb7a9d72b6595483fd629158025
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-30 21:54:29 -0400

    Update: added redis cache for processes data

--- src/controllers/processesController.ts
+++ src/controllers/processesController.ts
@@ -1,11 +1,25 @@
 import pool from '../db';
 import { Response, Request } from 'express';
 import { queryGetAllProcesses } from '../queries/queries';
+import { client, expiration } from '..';
 
 const getAllProcesses = async (req: Request, res: Response) => {
-  const coffeeProcesses = await pool.query(`${queryGetAllProcesses}`);
-
-  res.status(200).json(coffeeProcesses.rows);
+  try {
+    const cachedProcesses = await client.get('processes');
+    if (!cachedProcesses) {
+      const coffeeProcesses = await pool.query(`${queryGetAllProcesses}`);
+      await client.setEx(
+        'processes',
+        expiration,
+        JSON.stringify(coffeeProcesses.rows)
+      );
+      res.status(200).json(coffeeProcesses.rows);
+    } else {
+      res.status(200).json(JSON.parse(cachedProcesses));
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getAllProcesses };
