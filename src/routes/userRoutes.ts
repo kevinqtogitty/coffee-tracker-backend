d180176173423f98733ff462d0df3e49d126a49b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-04-06 21:38:25 -0400

    Update: removed deleted function import

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import {
   createAUser,
   deleteASingleUser,
-  getAllUsers,
   getASingleUser,
   updateUserData
 } from '../controllers/userController';
@@ -11,7 +10,6 @@ import { tryCatch } from '../utils/tryCatch';
 const userRouter = Router();
 
 userRouter.post('/', tryCatch(createAUser)); // create a user
-userRouter.get('/', tryCatch(getAllUsers)); // get all users
 userRouter.get('/:userId', tryCatch(getASingleUser)); // get single user
 userRouter.put('/:userId', tryCatch(updateUserData)); // update user data
 userRouter.delete('/:userId', tryCatch(deleteASingleUser)); // delete single user
